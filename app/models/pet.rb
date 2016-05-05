048bddcdf234690d741934a218ab4eb640ea8899
JonAbrego <abrego.fc@gmail.com>
2016-05-05 00:35:52 -0500

    Jugando con los mensaje

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ActiveRecord::Base
 
-	validates :name, :sex, :description, :age, :race, :height, :specie, presence: true
+	validates :name, :age, :race, :height, :specie, presence: true
+	validates :sex, presence: { message: "Si es Macho o Hembra" }
+	validates :description, presence: { message: "Debe ser algo descriptivo" }
 end
