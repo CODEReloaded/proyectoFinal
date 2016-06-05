85202cea81605fe07aebfcb9b530288ad96ce68f
yorchek <kuberjorg3n@hotmail.com>
2016-06-04 19:56:49 -0500

    mezclando ElegirND

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,10 +20,6 @@ class User < ActiveRecord::Base
   # Uncomment this for production
   # geocoded_by :ip_address
 
-  # This is for development environment, comment if your are in production
-  # geocoded_by :address
-  # after_validation :geocode
-
   # This is for fastness to find other users
   acts_as_mappable :auto_geocode => { :field=>:address, 
           :error_message=>'Direccion invalida, no se puede encontrar la longitud y latitud'},
